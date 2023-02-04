95dcaf177bac8865644cf326de6718b33686a090
Kurucz János <kuruczjanoshivatalos@gmail.com>
2023-02-04 15:46:49 +0100

    Version 1

--- src/app.js
+++ src/app.js
@@ -7,3 +7,22 @@
 * Github: https://github.com/KuruczJanos/fogyasztas.git
 * Licenc: GNU GPL
 */
+const kmnyitElem = document.querySelector('#kmnyit');
+const kmzarElem = document.querySelector('#kmzar');
+const tankolElem = document.querySelector('#tankol');
+const calcButton = document.querySelector('#calcButton');
+const acElem = document.querySelector('#ac');
+
+calcButton.addEventListener('click',() => {
+    let kmnyit = Number (kmnyitElem.value);
+    let kmzar = Number (kmzarElem.value);
+    let tankol = Number (tankolElem.value);
+    let ac = calcac (kmnyit, kmzar, tankol);
+    console.log(ac);
+    acElem.value = ac;
+});
+
+function calcac(kmnyit, kmzar, tankol){
+    let ac = (tankol/(kmzar-kmnyit))*100
+    return ac;
+}
